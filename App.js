63b1960c819b88fc6befec3a1170ff868e7bbaa7
Kaushik Sharma <90143986+ikaushiksharma@users.noreply.github.com>
2022-10-03 10:21:58 +0530

    Game Screen Ready

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
+import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const startGameHandler = (selectedNum) => {
+    setUserNumber(selectedNum);
+  };
+
+  const content = userNumber ? (
+    <GameScreen userChoice={userNumber} />
+  ) : (
+    <StartGameScreen startGame={startGameHandler} />
+  );
   return (
     <View style={styles.screen}>
       <Header title='Guess a Number' />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
---
+++ screens/GameScreen.js
@@ -0,0 +1,83 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
+import Card from '../components/Card';
+import NumberContainer from '../components/NumberContainer';
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndm = Math.floor(Math.random() * (max - min)) + min;
+  if (rndm == exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else return rndm;
+};
+
+const GameScreen = (props) => {
+  const currentHigh = useRef(100);
+  const currentLow = useRef(1);
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+  const [rounds, setRounds] = useState(0);
+  useEffect(() => {
+    if (currentGuess === props.userChoice) {
+      props.onGameOver(rounds);
+    }
+  }, [currentGuess, props.userChoice, props.onGameOver]);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === 'lower' && currentGuess < props.userChoice) ||
+      (direction === 'greater' && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't Lie!", 'You know that this is wrong...', [
+        { text: 'Sorry!', style: 'cancel' },
+      ]);
+      return;
+    }
+    if (direction === 'lower') {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNum = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNum);
+    setRounds((curRounds) => curRounds + 1);
+  };
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title='Lower' onPress={nextGuessHandler.bind(this, 'lower')} />
+        <Button
+          title='Greater'
+          onPress={nextGuessHandler.bind(this, 'greater')}
+        />
+      </Card>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    width: 300,
+    maxWidth: '80%',
+  },
+  button: {
+    width: 100,
+  },
+});
+export default GameScreen;
